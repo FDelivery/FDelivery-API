156d2a13b68cb53828f2d5f88c12ad5020801a75
HeadBang <50811873+m3et@users.noreply.github.com>
2021-04-19 13:57:46 +0300

    jwt user done

--- resources/Auth/jwt_manger.py
+++ resources/Auth/jwt_manger.py
@@ -7,6 +7,17 @@ def initialize_jwt(app):
     jwt.init_app(app)
 
 
+# Register a callback function that takes whatever object is passed in as the
+# identity when creating JWTs and converts it to a JSON serializable format.
+@jwt.user_identity_loader
+def user_identity_lookup(user):
+    return user.id
+
+
+# Register a callback function that loads a user from your database whenever
+# a protected route is accessed. This should return any python object on a
+# successful lookup, or None if the lookup failed for any reason (for example
+# if the user has been deleted from the database).
 @jwt.user_lookup_loader
 def user_lookup_callback(_jwt_header, jwt_data):
     from database.models.User import User
