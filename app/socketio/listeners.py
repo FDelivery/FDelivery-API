873c98b24c58af83c745a99bb6353dfdec8357d4
elchanan <elchananmahatsri@gmail.com>
2021-08-25 12:40:10 +0300

    notification bug fix

--- app/socketio/listeners.py
+++ app/socketio/listeners.py
@@ -1,16 +1,16 @@
-
+from flask import request
 
 from app.socketio import socketio
 from flask_socketio import join_room, leave_room
 
 
 @socketio.on('join')
-def register_delivery_room(b_id, sid):
-    join_room(b_id, sid=sid)
+def register_delivery_room(b_id):
+    join_room(b_id, sid=request.sid)
     print(b_id, "has joined the room")
 
 
 @socketio.on('leave')
-def register_delivery_room(b_id, sid):
-    leave_room(b_id, sid=sid)
+def register_delivery_room(b_id):
+    leave_room(b_id, sid=request.sid)
     print(b_id, "has left the room")
